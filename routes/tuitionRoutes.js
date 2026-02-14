caa1f2d4b7a4e7a0fd8a9499c0ecc55eff6b438d
MasumRezaMunna <masumrezamunna2005@gmail.com>
2026-02-14 11:08:57 +0600

    changed status

--- routes/tuitionRoutes.js
+++ routes/tuitionRoutes.js
@@ -6,7 +6,7 @@ const { verifyFirebaseToken, protect, restrictTo } = require("../middlewares/aut
 router.get("/all-open", async (req, res) => {
   try {
     const Tuition = require("../models/Tuition");
-    const tuitions = await Tuition.find({ status: "open" }).sort("-createdAt");
+    const tuitions = await Tuition.find({ status: "approved" }).sort("-createdAt");
     res.status(200).json({ status: "success", data: tuitions });
   } catch (err) {
     res.status(500).json({ message: err.message });
