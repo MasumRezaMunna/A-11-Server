6ecbdef74ff16fd09b7b1e937b9d3418798f6475
MasumRezaMunna <masumrezamunna2005@gmail.com>
2026-02-15 16:22:56 +0600

    remove app.listen from server.js

--- server.js
+++ server.js
@@ -46,4 +46,3 @@ const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
   res.send("Server is running perfectly!");
 });
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
